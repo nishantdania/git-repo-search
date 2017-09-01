af05bd556fe52fd00b47096131d7d72ddc87f99d
nishantdania <nishantdania@gmail.com>
2017-09-01 10:30:37 +0530

    search widget added

---
+++ src/components/SearchWidget/searchWidget.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import cx from 'classnames';
+import styles from './searchWidget.css';
+import { withRouter } from 'react-router-dom';
+
+class SearchWidget extends Component {
+
+  state = {
+    keyword: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+
+  handleKeyPress = (e) => {
+    const { history } = this.props;
+    const { keyword } = this.state;
+
+    if(e.which === 13 && keyword.length > 0) {
+      var url = '/search?q=' + keyword + '&page=1';
+      history.push(url);
+    }
+  }
+
+  render () {
+    return <div>
+      <input 
+        onChange={this.handleChange}
+        onKeyPress={this.handleKeyPress}
+      />
+    </div>
+  }
+
+}
+
+export default withRouter(SearchWidget);
