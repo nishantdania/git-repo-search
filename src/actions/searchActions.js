f304702ad286d430fde7bdb6d1ca6b11f40f63f1
nishantdania <nishantdania@gmail.com>
2017-09-01 12:27:21 +0530

    improved perceived perf

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,34 +1,64 @@
 import ApiCaller from './ApiCaller';
-import { SEARCH } from './constants';
-import { formatData } from '../utils/githubService';
+import { SEARCH } from './constants.js';
+import { formatData } from '../utils/githubService.js';
 
-export const search = (data) => (dispatch) => {
-  
-  dispatch({type: SEARCH.SEARCH_REQUEST});
+export const search = (data, onlyOnce = false) => (dispatch) => {
+
+  dispatch({
+    type: SEARCH.SEARCH_REQUEST,
+  });
+
+  if(!onlyOnce) {
+
+    ApiCaller.get(
+      '/search/repositories',
+      {
+        params: {
+          q: data.keyword,
+          page: data.page || 1,
+          per_page: SEARCH.PER_PAGE
+        }
+      })
+      .then((res) => {
+        res = formatData(res.data);
+        var result = {
+          keyword: data.keyword,
+          page: data.page,
+          result: res  
+        };
+        dispatch({
+          type: SEARCH.SEARCH_SUCCESS,
+          data: result
+        });
+      })
+      .catch((err) => {
+
+      });
+  }
 
   ApiCaller.get(
     '/search/repositories',
     {
       params: {
         q: data.keyword,
-        page: data.page || 1,
+        page: parseInt(data.page) + 1 || 2,
         per_page: SEARCH.PER_PAGE
       }
-    }
-  )
-  .then((res) => {
-    res = formatData(res.data);
-    var result = {
-      keyword: data.keyword,
-      page: data.page,
-      result: res
-    }
-    dispatch({
-      type: SEARCH.SEARCH_SUCCESS,
-      data: result
-    });
-  })
-  .catch((err) => {
+    })
+    .then((res) => {
+      res = formatData(res.data);
+      var result = {
+        keyword: data.keyword,
+        page: parseInt(data.page) + 1 || 2,
+        result: res
+      };
+      dispatch({
+        type: SEARCH.SEARCH_EXTRA_SUCCESS,
+        data: result
+      });
+    })
+    .catch((err) => {
 
-  });
+    });
+
 }
