89deba58f69e1eb90043bf0f406155fabd533885
nishantdania <nishantdania@gmail.com>
2017-09-01 10:22:03 +0530

    homepage component added

---
+++ src/components/Homepage/homepage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import cx from 'classnames';
+import styles from './homepage.css';
+
+class Homepage extends Component {
+
+  render () {
+    return <div>
+      Homepage
+    </div>
+  }
+
+}
+
+export default Homepage;
