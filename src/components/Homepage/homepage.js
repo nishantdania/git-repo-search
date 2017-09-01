b1bb15ce8a81fb90c16f77da3cdba72b8c4daa68
nishantdania <nishantdania@gmail.com>
2017-09-01 10:30:37 +0530

    search widget added

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import cx from 'classnames';
 import styles from './homepage.css';
+import SearchWidget from '../SearchWidget';
 
 class Homepage extends Component {
 
   render () {
     return <div>
-      Homepage
+      <SearchWidget />
     </div>
   }
 
